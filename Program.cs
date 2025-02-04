48c3d1b4cfc248ff1b19ab1263637135937022bf
339719916 <339719916@2260I-L29-029>
2025-02-04 14:04:07 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace topic_number_one
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("this will print on a new line");
+
+            Console.WriteLine();
+
+
+
+            Console.Write("Hello, World!");
+            Console.Write(" This Will print on the same line!");
+
+            Console.WriteLine("Something");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
